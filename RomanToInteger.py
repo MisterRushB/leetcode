7b682614745dc118955cba5296368d158aceeb93
Mustafa Khan <mm5khan@uwaterloo.ca>
2023-07-15 10:33:20 -0400

    Updated V2

--- RomanToInteger.py
+++ RomanToInteger.py
@@ -1,4 +1,5 @@
 def RomanToInteger(str): 
+    '''Converts Roman numerals to Integer values'''
     sum=0
     buffer=0
     # x=OrderedDict(("I",1),("V",5),("X",10),("L",50),("C",100),("D",500),("M",1000))
